a583de75c752396a466a2b39e472b9ad9e5ad90c
AlvifSandana <asm240800@gmail.com>
2021-12-19 21:28:56 +0700

    update controllers: get data metode pembayaran, akun pemasukan & pengeluaran

--- app/Controllers/Transaksi/PemasukanController.php
+++ app/Controllers/Transaksi/PemasukanController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers\Transaksi;
 
 use App\Controllers\BaseController;
+use App\Models\AkunPemasukan;
+use App\Models\MetodePembayaran;
 
 class PemasukanController extends BaseController
 {
@@ -12,6 +14,13 @@ class PemasukanController extends BaseController
         $request = \Config\Services::request();
         // get uri segment
         $data['uri_segment'] = $request->uri->getSegment(2);
+        // get data akun pemasukan
+        $m_ap = new AkunPemasukan();
+        $data['akun_pemasukan'] = $m_ap->findAll();
+        // get data metode pembayaran
+        $m_mp = new MetodePembayaran();
+        $data['metode_pembayaran'] = $m_mp->findAll();
+        // show view
         return view('pages/transaksi/pemasukan/index', $data);
     }
 }
--- app/Controllers/Transaksi/PengeluaranController.php
+++ app/Controllers/Transaksi/PengeluaranController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers\Transaksi;
 
 use App\Controllers\BaseController;
+use App\Models\AkunPengeluaran;
+use App\Models\MetodePembayaran;
 
 class PengeluaranController extends BaseController
 {
@@ -12,6 +14,13 @@ class PengeluaranController extends BaseController
         $request = \Config\Services::request();
         // get uri segment
         $data['uri_segment'] = $request->uri->getSegment(2);
+        // get data akun pengeluaran
+        $m_ap = new AkunPengeluaran();
+        $data['akun_pengeluaran'] = $m_ap->findAll();
+        // get data metode pembayaran
+        $m_mp = new MetodePembayaran();
+        $data['metode_pembayaran'] = $m_mp->findAll();
+        // show view
         return view('pages/transaksi/pengeluaran/index', $data);
     }
 }
