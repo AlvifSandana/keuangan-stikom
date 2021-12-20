d4925cca16c5df0c92c2f54ed90da02111618cb7
AlvifSandana <asm240800@gmail.com>
2021-12-20 15:15:01 +0700

    new migration: BiayaSKS

---
+++ app/Database/Migrations/2021-12-20-051822_CreateBiayaSKSTable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateBiayaSKSTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_sks' => [
+                'type' => 'VARCHAR',
+                'constraint' => 10,
+            ],
+            'angkatan_id' => [
+                'type' => 'VARCHAR',
+                'constraint' => 10
+            ],
+            'paket_id' => [
+                'type' => 'VARCHAR',
+                'constraint' => 10
+            ],
+            'biaya_sks' => [
+                'type' => 'INT'
+            ],
+            'created_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id_sks', true, true);
+        $this->forge->addForeignKey('angkatan_id', 'tbl_angkatan', 'id_angkatan', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('paket_id', 'tbl_paket', 'id_paket', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('tbl_biaya_sks');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tbl_biaya_sks');
+    }
+}
