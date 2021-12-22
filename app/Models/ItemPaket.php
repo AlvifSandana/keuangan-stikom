93a8a7f808c82f90bddac63a3798738708d7da53
AlvifSandana <asm240800@gmail.com>
2021-12-22 11:18:35 +0700

    ItemPaket model: update allowed_fields

--- app/Models/ItemPaket.php
+++ app/Models/ItemPaket.php
@@ -17,7 +17,7 @@ class ItemPaket extends Model
     protected $allowedFields    = [];
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
