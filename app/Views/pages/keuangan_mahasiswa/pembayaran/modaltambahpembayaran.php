513d2b2ddce4d475e8f8fdd77e4c19d38b1d8c4b
AlvifSandana <asm240800@gmail.com>
2021-12-24 16:03:11 +0700

    hide field emester_id & nama_item

--- app/Views/pages/keuangan_mahasiswa/pembayaran/modaltambahpembayaran.php
+++ app/Views/pages/keuangan_mahasiswa/pembayaran/modaltambahpembayaran.php
@@ -11,8 +11,8 @@
                 <form action="<?php echo base_url(); ?>/keuangan-mahasiswa/pembayaran/create" method="post" id="form_create_pembayaran" enctype="multipart/form-data">
                     <input type="number" name="kode_unit" id="add_kode_unit" value="<? echo $mahasiswa[0]['nim'] ?>" hidden>
                     <input type="number" name="user_id" id="add_user_id" value="<? echo session()->get('id_user'); ?>" hidden>
-                    <input type="text" name="semester_id" id="add_semester_id" value="" >
-                    <input type="text" name="nama_item" id="add_nama_item" value="" >
+                    <input type="text" name="semester_id" id="add_semester_id" value="" hidden>
+                    <input type="text" name="nama_item" id="add_nama_item" value="" hidden>
                     <div class="form-group">
                         <label for="itempembayaran">ITEM PAKET</label>
                         <select class="form-control" name="item_kode" id="add_item_kode">
