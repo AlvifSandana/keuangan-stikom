66a768c93adeedf11a8026d30cd00a225fbd36f6
AlvifSandana <asm240800@gmail.com>
2021-12-24 19:39:42 +0700

    login view: autoficus email field

--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -28,7 +28,7 @@
 
       <form action="<?= base_url() ?>/auth" method="post">
         <div class="input-group mb-3">
-          <input type="email" name="email" class="form-control" placeholder="Email">
+          <input type="email" name="email" class="form-control" placeholder="Email" autofocus>
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
