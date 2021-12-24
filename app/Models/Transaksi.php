f8876d8142158eb53fa351c8c5e0c6488155de7a
AlvifSandana <asm240800@gmail.com>
2021-12-24 15:59:49 +0700

    Transaksi Model: add allowed_field

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -14,7 +14,7 @@ class Transaksi extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['kode_transaksi', 'kode_unit', 'kategori_transaksi', 'item_kode', 'q_debit', 'q_kredit', 'kode_metode_pembayaran', 'bukti_transaksi', 'tanggal_transaksi'];
 
     // Dates
     protected $useTimestamps = true;
@@ -43,7 +43,7 @@ class Transaksi extends Model
     /**
      * Get data transaksi by kode_unit & kategori transaksi.
      */
-    public function findTransaksi(String $kode_unit, String $kategori_transaksi)
+    public function findTransaksi(String $kode_unit, String $kategori_transaksi, String $orderBy, String $direction)
     {
         try {
             // set result
@@ -52,7 +52,33 @@ class Transaksi extends Model
             $query = $this->builder()
                 ->like('kode_unit', $kode_unit, 'both')
                 ->like('kategori_transaksi', $kategori_transaksi)
-                ->orderBy('kode_transaksi', 'DESC')
+                ->orderBy($orderBy, $direction)
+                ->get();
+            // check query, when query success with data, 
+            // set to result
+            if ($query->getResultArray()) {
+                $result = $query->getResultArray();
+            }
+            return $result;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    /**
+     * Get data transaksi by kode_unit, kategori transaksi, dan item_kode.
+     */
+    public function findTransaksiByItemKode(String $kode_unit, String $kategori_transaksi, String $item_kode, String $orderBy, String $direction)
+    {
+        try {
+            // set result
+            $result = "Data tidak ditemukan!";
+            // set query
+            $query = $this->builder()
+                ->where('item_kode', $item_kode)
+                ->like('kode_unit', $kode_unit, 'both')
+                ->like('kategori_transaksi', $kategori_transaksi)
+                ->orderBy($orderBy, $direction)
                 ->get();
             // check query, when query success with data, 
             // set to result
