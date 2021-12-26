c02dbb53a652d5622c025df2ec984e8b7f10552b
AlvifSandana <asm240800@gmail.com>
2021-12-26 15:06:24 +0700

    TagihanController: create new tagihan

--- app/Controllers/Mahasiswa/TagihanController.php
+++ app/Controllers/Mahasiswa/TagihanController.php
@@ -3,16 +3,80 @@
 namespace App\Controllers\Mahasiswa;
 
 use App\Controllers\BaseController;
+use App\Models\Semester;
+use App\Models\Transaksi;
 
 class TagihanController extends BaseController
 {
     public function index()
     {
-        // create request instance
-        $request = \Config\Services::request();
-        // get uri segment for dynamic sidebar active item
-        $data['uri_segment'] = $request->uri->getSegment(2);
-        // show view
-        return view('pages/keuangan_mahasiswa/tagihan/index', $data);
+        //
+    }
+
+    /**
+     * Create new tagihan
+     */
+    public function create_tagihan()
+    {
+        try {
+            // create validator instance
+            $validator = \Config\Services::validation();
+            $validator->setRules([
+                'kode_unit' => 'required',
+                'q_kredit' => 'required',
+                'tanggal_transaksi' => 'required',
+            ]);
+            // begin validation
+            $isDataValid = $validator->withRequest($this->request)->run();
+            if ($isDataValid) {
+                // create model instance
+                $m_transaksi = new Transaksi();
+                // get last tagihan by nim
+                $last_tagihan = $m_transaksi->findTransaksi($this->request->getPost('kode_unit'), 'K', 'kode_transaksi', 'DESC');
+                // get semester
+                $current_semester = explode('SMT', $this->request->getPost('semester_id'));
+                // get last kode_transaksi
+                if ($last_tagihan != 'Data tidak ditemukan!') {
+                    $kode_transaksi = explode('-', $last_tagihan[0]['kode_transaksi']);
+                    $last_kode_transaksi = 'BY-' . $this->request->getPost('kode_unit') . '-K-' . number_format($current_semester[1]) . '-' . (1 + (int)$kode_transaksi[4]);
+                } else {
+                    $last_kode_transaksi = 'BY-' . $this->request->getPost('kode_unit') . '-K-' . number_format($current_semester[1]) . '-1';
+                }
+                // insert new tagihan
+                $new_tagihan = $m_transaksi->insert([
+                    'kode_transaksi' => $last_kode_transaksi,
+                    'kode_unit' => $this->request->getPost('kode_unit'),
+                    'kategori_transaksi' => 'K',
+                    'item_kode' => $this->request->getPost('item_kode'),
+                    'q_kredit' => $this->request->getPost('q_kredit'),
+                    'tanggal_transaksi' => $this->request->getPost('tanggal_transaksi')
+                ]);
+                if ($new_tagihan) {
+                    return json_encode([
+                        'status' => 'success',
+                        'message' => 'Berhasil menambahkan tagihan baru!',
+                        'data' => $new_tagihan,
+                    ]);
+                } else {
+                    return json_encode([
+                        'status' => 'failed',
+                        'message' => 'Gagal menambahkan tagihan baru.',
+                        'data' => ''
+                    ]);
+                }
+            } else {
+                return json_encode([
+                    'status' => 'failed',
+                    'message' => 'Validasi gagal, mohon cek field input!',
+                    'data' => $validator->getErrors(),
+                ]);
+            }
+        } catch (\Throwable $th) {
+            return json_encode([
+                'status' => 'error',
+                'message' => $th->getMessage(),
+                'data' => $th->getTrace(),
+            ]);
+        }
     }
 }
