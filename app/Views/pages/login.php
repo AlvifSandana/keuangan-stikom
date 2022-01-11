ed3eb24dac02f3eeab49651f5721b3e98a100c7a
AlvifSandana <asm240800@gmail.com>
2022-01-11 15:04:39 +0700

    change favicon

--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Login Sistem Keuangan</title>
-
+  <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
