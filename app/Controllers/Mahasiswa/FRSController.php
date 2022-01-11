ff7228113bb03e7dce34ff0dfa84129307692cdc
AlvifSandana <asm240800@gmail.com>
2022-01-11 12:10:23 +0700

    frs module: ongoing

--- app/Controllers/Mahasiswa/FRSController.php
+++ app/Controllers/Mahasiswa/FRSController.php
@@ -14,6 +14,7 @@ class FRSController extends BaseController
         $db_old = \Config\Database::connect('default_old');
         $builder_mhs = $db_old->table('tbl_mahasiswa');
         $builder_frs = $db_old->table('pw_tr_perwalian_header');
+        $builder_khs = $db_old->table('tbl_nilai_mahasiswa');
         $builder_frs_detail = $db_old->table('pw_tr_perwalian_detail');
         $builder_frs_dosenwali = $db_old->table('pw_tr_perwalian_header_dw');
         // get uri segment for dynamic sidebar active item
@@ -21,28 +22,76 @@ class FRSController extends BaseController
         $nim = $request->uri->getSegment(3);
         // get data perwalian mahasiswa
         $query_mhs = $builder_mhs
-            ->select('*, tbl_dosen_wali_mahasiswa.*, tbl_jurusan.*, tbl_paket.*')
+            ->select('*, tbl_dosen_wali_mahasiswa.*, tbl_jurusan.*, tbl_paket.*, tbl_dosen.*, tbl_status.status as status_mhs')
             ->where('tbl_mahasiswa.nim', $nim)
             ->join('tbl_dosen_wali_mahasiswa', 'tbl_mahasiswa.nim = tbl_dosen_wali_mahasiswa.nim', 'left')
+            ->join('tbl_dosen', 'tbl_dosen_wali_mahasiswa.kode_dosen = tbl_dosen.kode_dosen', 'inner')
             ->join('tbl_jurusan', 'tbl_mahasiswa.id_jur = tbl_jurusan.id_jur', 'left')
             ->join('tbl_paket', 'tbl_mahasiswa.id_paket = tbl_paket.id_paket', 'left')
             ->join('tbl_status', 'tbl_mahasiswa.status = tbl_status.id_sts', 'left')
             ->get();
+        // query get next semester
         $query_next_semester = $builder_frs
             ->select('semester, status')
             ->where('nim', $nim)
             ->get();
+        // query get data frs
         $query_frs = $builder_frs
             ->select("pw_tr_perwalian_header.nim, tbl_mahasiswa.nama_mhs, PesertaKelas.kode_mk, nama_mk, pw_tr_perwalian_header.semester as smtTempuh, semester as smtMK, jum_sks,jadwal,kapasitas, kode_dosen, nama_dosen, PesertaKelas.kelas, pw_tr_perwalian_header.status, pw_tr_perwalian_header.tgl_perwalian,PesertaKelas.Peserta, PesertaKelas.CalonPeserta")
-            ->join("(SELECT nim, tbl_mk.kode_mk, nama_mk, jum_sks, kelas, tbl_dosen.kode_dosen,nama_dosen FROM pw_tr_perwalian_detail LEFT JOIN tbl_mk ON pw_tr_perwalian_detail.kode_mk=tbl_mk.kode_mk LEFT JOIN tbl_dosen ON pw_tr_perwalian_detail.kode_dosen=tbl_dosen.kode_dosen) AS kl", "pw_tr_perwalian_header.nim=kl.nim", "left")
+            ->join("(SELECT nim, tbl_mk.kode_mk, nama_mk, sts,jum_sks, kelas, tbl_dosen.kode_dosen,nama_dosen FROM pw_tr_perwalian_detail LEFT JOIN tbl_mk ON pw_tr_perwalian_detail.kode_mk=tbl_mk.kode_mk LEFT JOIN tbl_dosen ON pw_tr_perwalian_detail.kode_dosen=tbl_dosen.kode_dosen) AS kl", "pw_tr_perwalian_header.nim=kl.nim", "left")
             ->join("tbl_mahasiswa", "pw_tr_perwalian_header.nim=tbl_mahasiswa.nim", "left")
             ->join("(SELECT pw_tr_perwalian_header.nim,jadwal,kapasitas,  pw_tr_perwalian_detail.kode_mk,  pw_tr_perwalian_detail.kelas,  SUM(CASE pw_tr_perwalian_header.Status AND pw_tr_perwalian_header_dw.Status WHEN '1' THEN 1 ELSE 0 END) AS Peserta, SUM(CASE pw_tr_perwalian_header.Status AND pw_tr_perwalian_header_dw.Status WHEN '0' THEN 1 ELSE 0 END) AS CalonPeserta  FROM pw_tr_perwalian_header  LEFT JOIN pw_tr_perwalian_detail  ON pw_tr_perwalian_header.nim = pw_tr_perwalian_detail.nim LEFT JOIN pw_tr_perwalian_header_dw  ON pw_tr_perwalian_header.nim = pw_tr_perwalian_header_dw.nim	 GROUP BY kode_mk,kelas) PesertaKelas", "kl.kode_mk = PesertaKelas.kode_mk AND kl.kelas = PesertaKelas.kelas", "left")
             ->where('pw_tr_perwalian_header.nim', $nim)
             ->orderBy('jadwal', 'DESC')
             ->get();
+        // query get data frs dosen wali
         $query_frs_dosenwali = $builder_frs
             ->where('nim', $nim)
             ->get();
+        // query get khs mahasiswa by nim
+        $next_semester = $query_next_semester->getResultArray();
+        $query_khs = $builder_khs
+            ->where('nim', $nim)
+            ->where('semester_ditempuh', ($next_semester[0]['semester'] - 1))
+            ->join('tbl_mk', 'tbl_nilai_mahasiswa.kode_mk = tbl_mk.kode_mk', 'left')
+            ->join('tbl_bobot_nilai', 'tbl_nilai_mahasiswa.grade = tbl_bobot_nilai.nilai')
+            ->join('tbl_dosen', 'tbl_nilai_mahasiswa.kode_dosen = tbl_dosen.kode_dosen', 'left')
+            ->get();
+        // get data tahun ajaran
+        $query_tahun_ajaran = $builder_frs_detail
+            ->select('kode_thn, kls_program')
+            ->where('nim', $nim)
+            ->groupBy('kode_thn')
+            ->get();
+        // set to view data
+        $data['khs'] = $query_khs->getResultArray();
+        $data['data_mhs'] = $query_mhs->getResultArray();
+        $data['list_frs_dw'] = $query_frs_dosenwali->getResultArray();
+        $data['next_semester'] = $query_next_semester->getResultArray();
+        $data['tahun_ajaran'] = $query_tahun_ajaran->getResultArray();
+        // get nilai mahasiswa
+        $total_sks = 0;
+        $total_nk = 0;
+        foreach ($query_khs->getResultArray() as $value) {
+            $total_sks += (int) $value['jum_sks'];
+            $total_nk += (int) $value['bobot'] * (int) $value['jum_sks'];
+        }
+        $sum_ipk = $total_sks != 0 ? $total_nk / $total_sks : 0;
+        $data['ipk'] = round($sum_ipk, 2);
+        $data['total_sks'] = $total_sks;
+        // get beban sks
+        if ($data['ipk'] >= 3) {
+            $data['beban_sks'] = 24;
+        } else if($data['ipk'] >= 2.75 && $data['ipk'] < 3) {
+            $data['beban_sks'] = 22;
+        } else if($data['ipk'] >= 2.51 && $data['ipk'] < 2.75) {
+            $data['beban_sks'] = 20;
+        } else if($data['ipk'] >= 2.00 && $data['ipk'] < 2.50) {
+            $data['beban_sks'] = 18;
+        } else {
+            $data['beban_sks'] = 15;
+        }
+        // pass data frs to view data
         if ($query_frs) {
             $data['list_frs'] = $query_frs->getResultArray();
         } else {
@@ -50,5 +99,17 @@ class FRSController extends BaseController
         }
         // show view
         return view('pages/keuangan_mahasiswa/frs/index', $data);
+    }
+
+    /**
+     * method acc FRS
+     */
+    public function acc_frs()
+    {
+        try {
+            
+        } catch (\Throwable $th) {
+            
+        }
     }
 }
--- app/Views/pages/keuangan_mahasiswa/frs/index.php
+++ app/Views/pages/keuangan_mahasiswa/frs/index.php
@@ -24,11 +24,87 @@
 <?= $this->section('content-body') ?>
 <section class="content">
     <div class="container-fluid">
+        <div class="row" mb-2>
+            <div class="col">
+                <div class="card">
+                    <div class="card-header border-0">
+                        <h4 class="card-title pt-2 mhs"><i class="fas fa-fw fa-user"></i> Data Mahasiswa</h4>
+                        <div class="card-tools float-right">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+                            <button type="button" class="btn btn-tool" data-card-widget="maximize"><i class="fas fa-expand"></i></button>
+                            <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <table class="table table-borderless">
+                                    <tr>
+                                        <td>NIM</td>
+                                        <td>:</td>
+                                        <td><?= $data_mhs[0]['nim'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Nama</td>
+                                        <td>:</td>
+                                        <td><?= $data_mhs[0]['nama_mhs'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Jurusan</td>
+                                        <td>:</td>
+                                        <td><?= $data_mhs[0]['jurusan'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Dosen Wali</td>
+                                        <td>:</td>
+                                        <td><?= $data_mhs[0]['nama_dosen'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Paket</td>
+                                        <td>:</td>
+                                        <td><?= $data_mhs[0]['nama_paket'] ?></td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <div class="col-md-6">
+                                <table class="table table-borderless">
+                                    <tr>
+                                        <td>Semester, Tahun Ajaran</td>
+                                        <td>:</td>
+                                        <td><?= $tahun_ajaran[0]['kode_thn'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>IP Semester Lalu</td>
+                                        <td>:</td>
+                                        <td><?= $ipk ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Program Kelas</td>
+                                        <td>:</td>
+                                        <td><?= $data_mhs[0]['program'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Semester yang akan ditempuh(*)</td>
+                                        <td>:</td>
+                                        <td><?= $next_semester[0]['semester']?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Beban Studi Maksimal</td>
+                                        <td>:</td>
+                                        <td><?= $beban_sks ?></td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="row mb-2">
             <div class="col">
                 <div class="card">
                     <div class="card-header border-0">
-                        <h4 class="card-title pt-2 mhs"><i class="fas fa-info-circle "></i> Mata Kuliah yang akan ditempuh pada semester ini</h4>
+                        <h4 class="card-title pt-2 mhs"><i class="fas fa-fw fa-info-circle "></i> Mata Kuliah yang akan ditempuh pada semester ini</h4>
                         <div class="card-tools float-right">
                             <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
                             <button type="button" class="btn btn-tool" data-card-widget="maximize"><i class="fas fa-expand"></i></button>
@@ -51,22 +127,34 @@
                                 <th>Calon</th>
                             </thead>
                             <tbody>
-                                <?php foreach ($list_frs as $key => $value) { ?>
-                                    <tr>
-                                        <td><?= $value['kode_mk'] ?></td>
-                                        <td><?= $value['nama_mk'] ?></td>
-                                        <td class="text-center"><?= $value['smtTempuh'] ?></td>
-                                        <td class="text-center"></td>
-                                        <td class="text-center"><?= $value['jum_sks'] ?></td>
-                                        <td><?= $value['nama_dosen'] ?></td>
-                                        <td class="text-center"><?= $value['kelas'] ?></td>
-                                        <td><?= $value['jadwal'] ?></td>
-                                        <td class="text-center"><?= $value['kapasitas'] ?></td>
-                                        <td class="text-center"><?= $value['Peserta'] ?></td>
-                                        <td class="text-center"><?= $value['CalonPeserta'] ?></td>
-                                    </tr>
-                                <? } ?>
+                            <?php foreach ($list_frs as $key => $value) { ?>
+                                <tr>
+                                    <td><?= $value['kode_mk'] ?></td>
+                                    <td><?= $value['nama_mk'] ?></td>
+                                    <td class="text-center"><?= $value['smtTempuh'] ?></td>
+                                    <td class="text-center"></td>
+                                    <td class="text-center"><?= $value['jum_sks'] ?></td>
+                                    <td><?= $value['nama_dosen'] ?></td>
+                                    <td class="text-center"><?= $value['kelas'] ?></td>
+                                    <td><?= $value['jadwal'] ?></td>
+                                    <td class="text-center"><?= $value['kapasitas'] ?></td>
+                                    <td class="text-center"><?= $value['Peserta'] ?></td>
+                                    <td class="text-center"><?= $value['CalonPeserta'] ?></td>
+                                </tr>
+                            <? } ?>
                             </tbody>
+                        </table>
+                        <table>
+                            <tr>
+                                <td>Total SKS</td>
+                                <td>:</td>
+                                <td class="font-weight-bold"><?= $total_sks ?></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>:</td>
+                                <td></td>
+                            </tr>
                         </table>
                     </div>
                 </div>
