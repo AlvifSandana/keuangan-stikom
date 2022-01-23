00280ef9f19df8c4f56c94468a9ab29a6638cec8
AlvifSandana <asm240800@gmail.com>
2022-01-23 11:26:23 +0700

    tagihan lain-lain: ongoing

--- app/Views/pages/master/keuangan/paket/index.php
+++ app/Views/pages/master/keuangan/paket/index.php
@@ -43,7 +43,10 @@
             <div class="col">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="h5 mb-4">Data Paket Tagihan <button class="btn btn-success float-right" data-toggle="modal" data-target="#modalAddPaket"><i class="fas fa-plus"></i> Tambah Paket</button></h5>
+                        <h5 class="h5 mb-4">Data Paket Tagihan
+                            <button class="btn btn-success float-right ml-2" data-toggle="modal" data-target="#modalAddItemPaket"><i class="fas fa-plus"></i> Tambah Item Tagihan</button>
+                            <button class="btn btn-success float-right" data-toggle="modal" data-target="#modalAddPaket"><i class="fas fa-plus"></i> Tambah Paket</button>
+                        </h5>
                         <select name="paket" class="form-control custom-select" id="select_paket" onchange="getItemPaket()" style="width: 100%;">
                             <option value=""></option>
                             <?php
@@ -52,8 +55,30 @@
                             }
                             ?>
                         </select>
-                        <h5 class="h5 mt-3 mb-4">Detail Item Paket <button class="btn btn-success float-right" data-toggle="modal" data-target="#modalAddItemPaket"><i class="fas fa-plus"></i> Tambah Item Tagihan</button></h5>
+                        <h5 class="h5 mt-3 mb-4">Detail Item Paket</h5>
                         <table class="table table-bordered tbl_master_paket" id="tbl_master_paket">
+                            <thead class="text-center">
+                                <th>Kode Item Tagihan</th>
+                                <th>Nama Item</th>
+                                <th>Tahun</th>
+                                <th>Semester</th>
+                                <th>Nominal</th>
+                                <th>Keterangan</th>
+                                <th>Action</th>
+                            </thead>
+                            <tbody class="">
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                    <h5 class="h5 mt-3 mb-4">Detail Item Tagihan Lain-lain</h5>
+                        <table class="table table-bordered tbl_master_paket" id="tbl_master_item_lain">
                             <thead class="text-center">
                                 <th>Kode Item Tagihan</th>
                                 <th>Nama Item</th>
