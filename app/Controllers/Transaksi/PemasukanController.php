a48f49c87b8d4d34b2cb122f4054caf5a637be34
AlvifSandana <asm240800@gmail.com>
2022-01-27 19:17:39 +0700

    PemasukanController: ongoing

--- app/Controllers/Transaksi/PemasukanController.php
+++ app/Controllers/Transaksi/PemasukanController.php
@@ -42,30 +42,100 @@ class PemasukanController extends BaseController
             $isDataValid = $validator->withRequest($this->request)->run();
             if ($isDataValid) {
                 // validation success
-                // slice tanggal_pemasukan
-                $tanggal_transaksi = explode('-', $this->request->getPost('tanggal_pemasukan'));
                 // create model instance
                 $m_transaksi = new Transaksi();
-                // create new data transaksi
-                $new_transaksi = $m_transaksi->insert([
-                    'kode_transaksi' => $this->request->getPost('kode_akun_pemasukan') . $tanggal_transaksi[1] . $tanggal_transaksi[0],
-                    'kode_unit' => '',
-                    'kategori_transaksi' => 'D',
-                    'q_debit' => '',
-                    'kode_metode_pembayaran' => '',
-                    'tanggal_transaksi' => $this->request->getPost('tanggal_pemasukan'),
-                    'keterangan_transaksi' => $this->request->getPost('keterangan_pemasukan')
-                ]);
+                // slice tanggal_pemasukan
+                $tanggal_transaksi = explode('-', $this->request->getPost('tanggal_pemasukan'));
+                // get bukti transaksi if existed
+                $is_bukti_transaksi = $this->request->getPost('is_bukti_transaksi');
+                $bukti_transaksi = $this->request->getFile('file') ? $this->request->getFile('file') : false;
+                // find previous transaksi
+                $prev_transaksi = $m_transaksi
+                    ->like('kode_transaksi', $this->request->getPost('kode_akun_pemasukan'))
+                    ->orderBy('id_transaksi', 'DESC')
+                    ->findAll();
+                if ($prev_transaksi && count($prev_transaksi) > 0) {
+                    // get previous kode_transaksi
+                    $current_kode_transaksi = explode('-', $prev_transaksi[0]['kode_transaksi']);
+                    // validate bukti transaksi
+                    if ($is_bukti_transaksi) {
+                        // validate dokumen pembayaran
+                        if (!$bukti_transaksi->isValid()) {
+                            // throw error 
+                            throw new \RuntimeException($bukti_transaksi->getErrorString() . '(' . $bukti_transaksi->getError() . ')');
+                            $result = [
+                                'status' => 'error',
+                                'message' => $bukti_transaksi->getErrorString() . '(' . $bukti_transaksi->getError() . ')',
+                                'data' => []
+                            ];
+                            return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', $result['message']);
+                        }
+                        // prevent from tampering upload file .php (backdoor)
+                        $validate_file_type = $bukti_transaksi->getMimeType();
+                        switch ($validate_file_type) {
+                            case 'text/php':
+                                return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', 'File validation failed!');
+                                break;
+                            case 'text/x-php':
+                                return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', 'File validation failed!');
+                                break;
+                            case 'application/php':
+                                return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', 'File validation failed!');
+                                break;
+                            case 'application/x-php':
+                                return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', 'File validation failed!');
+                                break;
+                            case 'application/x-httpd-php':
+                                return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', 'File validation failed!');
+                                break;
+                            case 'application/x-httpd-php-source':
+                                return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', 'File validation failed!');
+                                break;
+                        }
+                        // random filename
+                        $fn = $this->request->getPost('kode_akun_pemasukan') . '_' . $bukti_transaksi->getRandomName();
+                        // move file to public/uploaded/bukti_transaksi
+                        $upload_path = $bukti_transaksi->move(ROOTPATH . 'public/uploaded/bukti_transaksi/', $fn);
+                        // create new data transaksi
+                        $new_transaksi = $m_transaksi->insert([
+                            'kode_transaksi' => $this->request->getPost('kode_akun_pemasukan') . '-' . $tanggal_transaksi[1] . '-' . $tanggal_transaksi[0] . '-' . '000' . ((int)$current_kode_transaksi[4] + 1),
+                            'kode_unit' => $this->request->getPost('kode_akun_pemasukan'),
+                            'kategori_transaksi' => 'D',
+                            'q_debit' => $this->request->getPost('nominal_pemasukan'),
+                            'kode_metode_pembayaran' => $this->request->getPost('metode_pembayaran'),
+                            'tanggal_transaksi' => $this->request->getPost('tanggal_pemasukan'),
+                            'keterangan_transaksi' => $this->request->getPost('keterangan_pemasukan'),
+                            'bukti_transaksi' => $fn,
+                        ]);
+                    } else {
+                        // create new data transaksi
+                        $new_transaksi = $m_transaksi->insert([
+                            'kode_transaksi' => $this->request->getPost('kode_akun_pemasukan') . '-' . $tanggal_transaksi[1] . '-' . $tanggal_transaksi[0] . '-' . '000' . ((int)$current_kode_transaksi[4] + 1),
+                            'kode_unit' => $this->request->getPost('kode_akun_pemasukan'),
+                            'kategori_transaksi' => 'D',
+                            'q_debit' => $this->request->getPost('nominal_pemasukan'),
+                            'kode_metode_pembayaran' => $this->request->getPost('metode_pembayaran'),
+                            'tanggal_transaksi' => $this->request->getPost('tanggal_pemasukan'),
+                            'keterangan_transaksi' => $this->request->getPost('keterangan_pemasukan')
+                        ]);
+                    }
+                } else {
+                    // first data, create data with first numbering
+                    // validate bukti transaksi
+                    if ($is_bukti_transaksi) {
+                    } else {
+                    }
+                }
                 if ($new_transaksi) {
                     return json_encode([
-                        'status' => '',
-                        'message' => '',
+                        'status' => 'success',
+                        'message' => 'Berhasil menambahkan pemasukan!',
                         'data' => ''
                     ]);
                 } else {
                     return json_encode([
-                        'status' => '',
-                        'message' => '',
+                        'status' => 'failed',
+                        'message' => 'Gagal menambahkan transaksi!',
                         'data' => ''
                     ]);
                 }
