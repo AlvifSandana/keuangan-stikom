0331466d2efc7bd16d21cf0c278fe4f09b9c9a1a
AlvifSandana <asm240800@gmail.com>
2022-01-29 19:59:03 +0700

    Pemasukan module: almost done :)

--- app/Controllers/Transaksi/PemasukanController.php
+++ app/Controllers/Transaksi/PemasukanController.php
@@ -7,6 +7,8 @@ use App\Models\AkunPemasukan;
 use App\Models\MetodePembayaran;
 use App\Models\Transaksi;
 
+use function PHPUnit\Framework\isType;
+
 class PemasukanController extends BaseController
 {
     public function index()
@@ -25,9 +27,30 @@ class PemasukanController extends BaseController
         return view('pages/transaksi/pemasukan/index', $data);
     }
 
+    public function get_current_kode_transaksi(String $kode_akun_pemasukan)
+    {
+        // create model instance
+        $m_transaksi = new Transaksi();
+        // get previous transaksi
+        $prev_transaksi = $m_transaksi
+            ->like('kode_transaksi', $kode_akun_pemasukan)
+            ->orderBy('id_transaksi', 'DESC')
+            ->findAll();
+        // check
+        if (count($prev_transaksi) > 0) {
+            // get previous kode_transaksi
+            $current_kode_transaksi = explode('-', $prev_transaksi[0]['kode_transaksi']);
+            return $current_kode_transaksi;
+        } else {
+            return '0001';
+        }
+    }
+
     public function create_pemasukan()
     {
         try {
+            helper(['custom_helper']);
+            $insert_data = [];
             // create validator
             $validator = \Config\Services::validation();
             // validator rules
@@ -36,7 +59,7 @@ class PemasukanController extends BaseController
                 'tanggal_pemasukan' => 'required',
                 'nominal_pemasukan' => 'required',
                 'metode_pembayaran' => 'required',
-                'keterangan_transaksi' => 'required',
+                #'keterangan_transaksi' => 'required',
             ]);
             // begin validation
             $isDataValid = $validator->withRequest($this->request)->run();
@@ -49,109 +72,108 @@ class PemasukanController extends BaseController
                 // get bukti transaksi if existed
                 $is_bukti_transaksi = $this->request->getPost('is_bukti_transaksi');
                 $bukti_transaksi = $this->request->getFile('file') ? $this->request->getFile('file') : false;
-                // find previous transaksi
-                $prev_transaksi = $m_transaksi
-                    ->like('kode_transaksi', $this->request->getPost('kode_akun_pemasukan'))
-                    ->orderBy('id_transaksi', 'DESC')
-                    ->findAll();
-                if ($prev_transaksi && count($prev_transaksi) > 0) {
-                    // get previous kode_transaksi
-                    $current_kode_transaksi = explode('-', $prev_transaksi[0]['kode_transaksi']);
-                    // validate bukti transaksi
-                    if ($is_bukti_transaksi) {
-                        // validate dokumen pembayaran
-                        if (!$bukti_transaksi->isValid()) {
-                            // throw error 
-                            throw new \RuntimeException($bukti_transaksi->getErrorString() . '(' . $bukti_transaksi->getError() . ')');
-                            $result = [
-                                'status' => 'error',
-                                'message' => $bukti_transaksi->getErrorString() . '(' . $bukti_transaksi->getError() . ')',
-                                'data' => []
-                            ];
-                            return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', $result['message']);
-                        }
-                        // prevent from tampering upload file .php (backdoor)
-                        $validate_file_type = $bukti_transaksi->getMimeType();
-                        switch ($validate_file_type) {
-                            case 'text/php':
-                                return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', 'File validation failed!');
-                                break;
-                            case 'text/x-php':
-                                return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', 'File validation failed!');
-                                break;
-                            case 'application/php':
-                                return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', 'File validation failed!');
-                                break;
-                            case 'application/x-php':
-                                return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', 'File validation failed!');
-                                break;
-                            case 'application/x-httpd-php':
-                                return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', 'File validation failed!');
-                                break;
-                            case 'application/x-httpd-php-source':
-                                return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', 'File validation failed!');
-                                break;
-                        }
-                        // random filename
-                        $fn = $this->request->getPost('kode_akun_pemasukan') . '_' . $bukti_transaksi->getRandomName();
-                        // move file to public/uploaded/bukti_transaksi
-                        $upload_path = $bukti_transaksi->move(ROOTPATH . 'public/uploaded/bukti_transaksi/', $fn);
-                        // create new data transaksi
-                        $new_transaksi = $m_transaksi->insert([
-                            'kode_transaksi' => $this->request->getPost('kode_akun_pemasukan') . '-' . $tanggal_transaksi[1] . '-' . $tanggal_transaksi[0] . '-' . '000' . ((int)$current_kode_transaksi[4] + 1),
-                            'kode_unit' => $this->request->getPost('kode_akun_pemasukan'),
-                            'kategori_transaksi' => 'D',
-                            'q_debit' => $this->request->getPost('nominal_pemasukan'),
-                            'kode_metode_pembayaran' => $this->request->getPost('metode_pembayaran'),
-                            'tanggal_transaksi' => $this->request->getPost('tanggal_pemasukan'),
-                            'keterangan_transaksi' => $this->request->getPost('keterangan_pemasukan'),
-                            'bukti_transaksi' => $fn,
-                        ]);
-                    } else {
-                        // create new data transaksi
-                        $new_transaksi = $m_transaksi->insert([
-                            'kode_transaksi' => $this->request->getPost('kode_akun_pemasukan') . '-' . $tanggal_transaksi[1] . '-' . $tanggal_transaksi[0] . '-' . '000' . ((int)$current_kode_transaksi[4] + 1),
-                            'kode_unit' => $this->request->getPost('kode_akun_pemasukan'),
-                            'kategori_transaksi' => 'D',
-                            'q_debit' => $this->request->getPost('nominal_pemasukan'),
-                            'kode_metode_pembayaran' => $this->request->getPost('metode_pembayaran'),
-                            'tanggal_transaksi' => $this->request->getPost('tanggal_pemasukan'),
-                            'keterangan_transaksi' => $this->request->getPost('keterangan_pemasukan')
-                        ]);
+                // find previous transaksi and get current kode transaksi
+                $current_kode_transaksi = $this->get_current_kode_transaksi($this->request->getPost('kode_akun_pemasukan'));
+                // check current kode transaksi and bukti pembayaran
+                if (is_array($current_kode_transaksi) && $is_bukti_transaksi) {
+                    // validate dokumen pembayaran
+                    if (!$bukti_transaksi->isValid()) {
+                        // throw error 
+                        throw new \RuntimeException($bukti_transaksi->getErrorString() . '(' . $bukti_transaksi->getError() . ')');
+                        $result = [
+                            'status' => 'error',
+                            'message' => $bukti_transaksi->getErrorString() . '(' . $bukti_transaksi->getError() . ')',
+                            'data' => []
+                        ];
+                        return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', $result['message']);
                     }
-                } else {
+                    // prevent from tampering upload file .php (backdoor)
+                    $file_type = $bukti_transaksi->getMimeType();
+                    $validate_file_type = check_file_type($file_type);
+                    if ($validate_file_type == true || is_string($validate_file_type)) {
+                        return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', 'File type is not valid!');
+                    }
+                    // random filename
+                    $fn = $this->request->getPost('kode_akun_pemasukan') . '_' . $bukti_transaksi->getRandomName();
+                    // move file to public/uploaded/bukti_transaksi
+                    $upload_path = $bukti_transaksi->move(ROOTPATH . 'public/uploaded/bukti_transaksi/', $fn);
+                    // set insert data
+                    $insert_data['kode_transaksi'] = $this->request->getPost('kode_akun_pemasukan') . '-' . $tanggal_transaksi[1] . '-' . $tanggal_transaksi[0] . '-' . '000' . ((int)$current_kode_transaksi[4] + 1);
+                    $insert_data['bukti_transaksi'] = $fn;
+                } else if (is_string($current_kode_transaksi) && $is_bukti_transaksi) {
                     // first data, create data with first numbering
-                    // validate bukti transaksi
-                    if ($is_bukti_transaksi) {
-                    } else {
+                    // validate dokumen pembayaran
+                    if (!$bukti_transaksi->isValid()) {
+                        // throw error 
+                        throw new \RuntimeException($bukti_transaksi->getErrorString() . '(' . $bukti_transaksi->getError() . ')');
+                        $result = [
+                            'status' => 'error',
+                            'message' => $bukti_transaksi->getErrorString() . '(' . $bukti_transaksi->getError() . ')',
+                            'data' => []
+                        ];
+                        return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', $result['message']);
                     }
-                }
-                if ($new_transaksi) {
-                    return json_encode([
-                        'status' => 'success',
-                        'message' => 'Berhasil menambahkan pemasukan!',
-                        'data' => ''
-                    ]);
+                    // prevent from tampering upload file .php (backdoor)
+                    $file_type = $bukti_transaksi->getMimeType();
+                    $validate_file_type = check_file_type($file_type);
+                    if ($validate_file_type == true || is_string($validate_file_type)) {
+                        return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', 'File type is not valid!');
+                    }
+                    // random filename
+                    $fn = $this->request->getPost('kode_akun_pemasukan') . '_' . $bukti_transaksi->getRandomName();
+                    // move file to public/uploaded/bukti_transaksi
+                    $upload_path = $bukti_transaksi->move(ROOTPATH . 'public/uploaded/bukti_transaksi/', $fn);
+                    // set insert data
+                    $insert_data['kode_transaksi'] = $this->request->getPost('kode_akun_pemasukan') . '-' . $tanggal_transaksi[1] . '-' . $tanggal_transaksi[0] . '-' . $current_kode_transaksi;
+                    $insert_data['bukti_transaksi'] = $fn;
+                } else if (is_array($current_kode_transaksi)) {
+                    // without bukti transaksi
+                    $insert_data['kode_transaksi'] = $this->request->getPost('kode_akun_pemasukan') . '-' . $tanggal_transaksi[1] . '-' . $tanggal_transaksi[0] . '-' . '000' . ((int)$current_kode_transaksi[4] + 1);
                 } else {
-                    return json_encode([
-                        'status' => 'failed',
-                        'message' => 'Gagal menambahkan transaksi!',
-                        'data' => ''
-                    ]);
+                    // without bukti transaksi
+                    $insert_data['kode_transaksi'] = $this->request->getPost('kode_akun_pemasukan') . '-' . $tanggal_transaksi[1] . '-' . $tanggal_transaksi[0] . '-' . $current_kode_transaksi;
+                }
+                // set insert data
+                $insert_data['kode_unit'] = $this->request->getPost('kode_akun_pemasukan');
+                $insert_data['kategori_transaksi'] = 'D';
+                $insert_data['q_debit'] = $this->request->getPost('nominal_pemasukan');
+                $insert_data['kode_metode_pembayaran'] = $this->request->getPost('metode_pembayaran');
+                $insert_data['tanggal_transaksi'] = $this->request->getPost('tanggal_pemasukan');
+                $insert_data['keterangan_transaksi'] = $this->request->getPost('keterangan_pemasukan');
+                // create new data transaksi
+                dd($insert_data);
+                $new_transaksi = $m_transaksi->insert($insert_data);
+                
+                if ($new_transaksi) {
+                    // return json_encode([
+                    //     'status' => 'success',
+                    //     'message' => 'Berhasil menambahkan pemasukan!',
+                    //     'data' => ''
+                    // ]);
+                    return redirect()->to(base_url() . '/transaksi/pemasukan')->with('success', 'File type is not valid!');
+                } else {
+                    // return json_encode([
+                    //     'status' => 'failed',
+                    //     'message' => 'Gagal menambahkan transaksi!',
+                    //     'data' => ''
+                    // ]);
+                    return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', 'Gagal menambahkan transaksi');
                 }
             } else {
-                return json_encode([
-                    'status' => 'failed',
-                    'message' => 'Validasi gagal, mohon isi field dengan benar!',
-                    'data' => $validator->getErrors()
-                ]);
+                // return json_encode([
+                //     'status' => 'failed',
+                //     'message' => 'Validasi gagal, mohon isi field dengan benar!',
+                //     'data' => $validator->getErrors()
+                // ]);
+                return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', 'Data is not valid!\n'.implode($validator->getErrors()));
             }
         } catch (\Throwable $th) {
-            return json_encode([
-                'status' => 'error',
-                'message' => $th->getMessage(),
-                'data' => $th->getTraceAsString()
-            ]);
+            // return json_encode([
+            //     'status' => 'error',
+            //     'message' => $th->getMessage(),
+            //     'data' => $th->getTraceAsString()
+            // ]);
+            return redirect()->to(base_url() . '/transaksi/pemasukan')->with('error', $th->getMessage());
         }
     }
 }
--- app/Views/pages/transaksi/pemasukan/index.php
+++ app/Views/pages/transaksi/pemasukan/index.php
@@ -40,16 +40,17 @@
 <?= $this->section('content-body') ?>
 <section class="content">
     <div class="container-fluid">
+        <?= $this->include('layout/flash') ?>
         <div class="row mb-2">
             <div class="col">
                 <div class="card">
                     <div class="card-body">
                         <h4 class="h4">Input Pemasukan</h4>
                         <hr>
-                        <form action="<?php echo base_url(); ?>/pembayaran/create" method="post" id="form_create_pembayaran" enctype="multipart/form-data">
+                        <form action="<?= base_url() ?>/transaksi/pemasukan/create" method="post" id="form_create_pemasukan" enctype="multipart/form-data">
                             <div class="form-group">
                                 <label for="itempembayaran">Kategori Pemasukan</label>
-                                <select class="form-control custom-select" name="kode_akun_pemasukam" id="kode_akun_pemasukan" style="width: 100%;">
+                                <select class="form-control custom-select" name="kode_akun_pemasukan" id="kode_akun_pemasukan" style="width: 100%;">
                                     <?php foreach ($akun_pemasukan as $p) {
                                         echo '<option value="' . $p["kode_akun"] . '">' . $p["kode_akun"] . ' - ' . $p["nama_akun"] . '</option>';
                                     } ?>
@@ -57,7 +58,7 @@
                             </div>
                             <div class="form-group">
                                 <label for="tanggal_pembayaran">Tanggal Pemasukan</label>
-                                <input type="date" class="form-control" name="tanggal_pemasukam" id="tanggal_pemasukan">
+                                <input type="date" class="form-control" name="tanggal_pemasukan" id="tanggal_pemasukan">
                             </div>
                             <div class="form-group">
                                 <label for="">Nominal Pemasukan</label>
@@ -65,7 +66,7 @@
                                     <div class="input-group-prepend">
                                         <span class="input-group-text">Rp. </span>
                                     </div>
-                                    <input type="number" class="form-control" name="nominal_pembayaran" id="add_nominal_pembayaran" aria-label="NOMINAL PEMBAYARAN">
+                                    <input type="number" class="form-control" name="nominal_pemasukan" id="add_nominal_pemmasukan" aria-label="NOMINAL PEMBAYARAN">
                                     <div class="input-group-append">
                                         <span class="input-group-text">.00</span>
                                     </div>
@@ -73,7 +74,7 @@
                             </div>
                             <div class="form-group">
                                 <label for="itempembayaran">Metode Pembayaran</label>
-                                <select class="form-control custom-select" name="kode_metode_pembayaran" id="kode_metode_pembayaran" style="width: 100%;">
+                                <select class="form-control custom-select" name="metode_pembayaran" id="metode_pembayaran" style="width: 100%;">
                                     <?php foreach ($metode_pembayaran as $mp) {
                                         echo '<option value="' . $mp["id_metode"] . '">' . $mp["id_metode"] . ' - ' . $mp["nama_metode_pembayaran"] . '</option>';
                                     } ?>
@@ -81,12 +82,12 @@
                             </div>
                             <div class="form-group">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="1" name="is_dokumen_pembayaran" id="is_fbp">
+                                    <input class="form-check-input" type="checkbox" value="1" name="is_bukti_transaksi" id="is_fbp">
                                     <label class="" for="">Bukti Transaksi</label>
                                 </div>
                                 <div class="input-group mb-3">
                                     <div class="custom-file">
-                                        <input type="file" class="custom-file-input" id="fbp" name="dokumen_pembayaran" disabled>
+                                        <input type="file" class="custom-file-input" id="fbp" name="bukti_transaksi" disabled>
                                         <label class="custom-file-label" for="inputGroupFile01">Choose file...</label>
                                     </div>
                                 </div>
