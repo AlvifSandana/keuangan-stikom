f2d5559e963a4daf3ee62e1cf326fbaab6b91d1a
AlvifSandana <asm240800@gmail.com>
2022-02-02 17:47:04 +0700

    Akun Pengeluaran module: ongoing

---
+++ app/Controllers/Master/AkunPengeluaranController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\Master;
+
+use App\Controllers\BaseController;
+
+class AkunPengeluaranController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
