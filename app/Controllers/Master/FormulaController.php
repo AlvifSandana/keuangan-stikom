135800941ce304f61bed32dad128096cfe26087b
AlvifSandana <asm240800@gmail.com>
2022-02-04 21:36:24 +0700

    Formula module: on going

--- app/Controllers/Master/FormulaController.php
+++ app/Controllers/Master/FormulaController.php
@@ -4,6 +4,7 @@ namespace App\Controllers\Master;
 
 use App\Controllers\BaseController;
 use App\Models\Formula;
+use PhpParser\Node\Stmt\For_;
 
 class FormulaController extends BaseController
 {
@@ -18,5 +19,141 @@ class FormulaController extends BaseController
         $data['formula'] = $m_formula->findAll();
         // get data akun pemasukan
         return view('pages/master/keuangan/formula/index', $data);
+    }
+
+    /**
+     * create new formula
+     */
+    public function create_formula()
+    {
+        try {
+            // create validator instance
+            $validator = \Config\Services::validation();
+            // set validator rules
+            $validator->setRules([
+                'item_kode' => 'required',
+                'persentase' => 'required',
+            ]);
+            // begin validation
+            $isDataValid = $validator->withRequest($this->request)->run();
+            if ($isDataValid) {
+                // create model instance
+                $m_formula = new Formula();
+                // get previous kode_formula
+                $prev_formula = $m_formula->orderBy('id_formula', 'DESC')->findAll();
+                // check
+                if (count($prev_formula) > 0) {
+                    // get kode formula
+                    $prev_kode_formula = explode('FORMULA', $prev_formula[0]['kode_formula']);
+                    // insert new data
+                    $insert_data = $m_formula->insert([
+                        'kode_formula' => 'FORMULA' . ((int)$prev_kode_formula[1] + 1),
+                        'item_kode' => $this->request->getPost('item_kode'),
+                        'persentase' => $this->request->getPost('presentase')
+                    ]);
+                    // check
+                    if ($insert_data) {
+                        return json_encode([
+                            'status' => 'success',
+                            'message' => 'Berhasil menambahkan formula!',
+                            'data' => $insert_data
+                        ]);
+                    } else {
+                        return json_encode([
+                            'status' => 'failed',
+                            'message' => 'Gagal menambahkan formula!',
+                            'data' => []
+                        ]);
+                    }
+                } else {
+                    // insert new data
+                    $insert_data = $m_formula->insert([
+                        'kode_formula' => 'FORMULA1',
+                        'item_kode' => $this->request->getPost('item_kode'),
+                        'persentase' => $this->request->getPost('presentase')
+                    ]);
+                    // check
+                    if ($insert_data) {
+                        return json_encode([
+                            'status' => 'success',
+                            'message' => 'Berhasil menambahkan formula!',
+                            'data' => $insert_data
+                        ]);
+                    } else {
+                        return json_encode([
+                            'status' => 'failed',
+                            'message' => 'Gagal menambahkan formula!',
+                            'data' => []
+                        ]);
+                    }
+                }
+            } else {
+                return json_encode([
+                    'status' => 'failed',
+                    'message' => 'Validasi gagal, silahkan isi form dengan benar',
+                    'data' => $validator->getErrors()
+                ]);
+            }
+        } catch (\Throwable $th) {
+            return json_encode([
+                'status' => 'error',
+                'message' => $th->getMessage(),
+                'data' => $th->getTrace()
+            ]);
+        }
+    }
+
+    /**
+     * update formula
+     */
+    public function update_formula()
+    {
+        try {
+            // create validator instance
+            $validator = \Config\Services::validation();
+            // set validator rules
+            $validator->setRules([
+                'id_formula' => 'required',
+                'kode_formula' => 'required',
+                'item_kode' => 'required',
+                'presentase' => 'required',
+            ]);
+            // begin validation
+            $isDataValid = $validator->withRequest($this->require)->run();
+            if ($isDataValid) {
+                // create model instance
+                $m_formula = new Formula();
+                // update data
+                $update_data = $m_formula->update($this->request->getPost('id_formula'), [
+                    'persentase' => $this->request->getPost('persentase'),
+                ]);
+                // check
+                if ($update_data) {
+                    return json_encode([
+                        'status' => 'success',
+                        'message'=> 'Berhasil memperbarui data formula!',
+                        'data' => $update_data
+                    ]);
+                } else {
+                    return json_encode([
+                        'status' => 'failed',
+                        'message'=> 'Gagal memperbarui data formula!',
+                        'data' => []
+                    ]);
+                }
+            } else {
+                return json_encode([
+                    'status' => 'failed',
+                    'message'=> 'Validasi gagal, mohon isi form dengan benar!',
+                    'data' => $validator->getErrors()
+                ]);
+            }
+        } catch (\Throwable $th) {
+            return json_encode([
+                'status' => 'error',
+                'message'=> $th->getMessage(),
+                'data' => $th->getTrace()
+            ]);
+        }
     }
 }
