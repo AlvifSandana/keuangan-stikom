e8348edf355f1ace0f5eb3a21f7aced70e3ff272
AlvifSandana <asm240800@gmail.com>
2022-03-01 09:36:24 +0700

    PembayaranVAController: ongoing

--- app/Controllers/Mahasiswa/PembayaranVAController.php
+++ app/Controllers/Mahasiswa/PembayaranVAController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers\Mahasiswa;
 
 use App\Controllers\BaseController;
+use PhpOffice\PhpSpreadsheet\Reader\Xls;
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 
 class PembayaranVAController extends BaseController
 {
@@ -14,5 +16,38 @@ class PembayaranVAController extends BaseController
         $data['uri_segment'] = $request->uri->getSegment(2);
         // show view
         return view('pages/keuangan_mahasiswa/pembayaran-va/index', $data);
+    }
+
+    /**
+     * Upload VA file
+     */
+    public function upload_va()
+    {
+        try {
+            // get file from POST requst
+            $file = $this->request->getFile('file_va');
+            // validate uploaded file
+            if (!$file->isValid()) {
+                // throw error 
+                throw new \RuntimeException($file->getErrorString() . '(' . $file->getError() . ')');
+                return redirect()
+                    ->to(base_url() . '/keuangan-mahasiswa/pembayaran-va')
+                    ->with('error', $file->getErrorString() . '(' . $file->getError() . ')');
+            } else {
+                if (! $file->hasMoved()) {
+                    // create reader
+                    if ($file->getClientExtension() == 'xls') {
+                        $reader = new Xls();
+                    } else {
+                        $reader = new Xlsx();
+                    }
+                    // read file
+                    $spreadsheet = $reader->load($file);
+                    
+                }
+            }
+        } catch (\Throwable $th) {
+            return redirect()->to(base_url().'/keuangan-mahasiswa/pembayaran-va')->with('error', $th->getMessage());
+        }
     }
 }
