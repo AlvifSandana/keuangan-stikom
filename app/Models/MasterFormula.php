ae5b117686336a0e179075f17b53fa0f83f8a5b9
AlvifSandana <asm240800@gmail.com>
2022-03-01 11:13:03 +0700

    new Model: MasterFormula

---
+++ app/Models/MasterFormula.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class MasterFormula extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'tbl_master_formula';
+    protected $primaryKey       = 'kode_mformula';
+    protected $useAutoIncrement = false;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['id_mformula', 'kode_mformula', 'persentase_tw', 'persentase_tb'];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
