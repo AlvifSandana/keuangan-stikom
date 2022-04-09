feacf7dae636c0652fd16f534d32514bf81e23e3
AlvifSandana <asm240800@gmail.com>
2022-04-09 14:30:51 +0700

    update migration: ON UPDATE CASCADE

--- app/Database/Migrations/2022-04-09-023148_AddAngkatanidAfterKeteranganpaket.php
+++ app/Database/Migrations/2022-04-09-023148_AddAngkatanidAfterKeteranganpaket.php
@@ -10,14 +10,14 @@ class AddAngkatanidAfterKeteranganpaket extends Migration
     {
         $field = [
             'angkatan_id' => ['type' => 'VARCHAR', 'constraint' => 10,'null' => true, 'after' => 'keterangan_paket'],
-            'CONSTRAINT tbl_paket_angkatan_id_foreign FOREIGN KEY(`angkatan_id`) REFERENCES `tbl_angkatan`(`id_angkatan`)'
+            'CONSTRAINT tbl_paket_angkatan_id_foreign FOREIGN KEY(`angkatan_id`) REFERENCES `tbl_angkatan`(`id_angkatan`) ON UPDATE CASCADE'
         ];
         $this->forge->addColumn('tbl_paket', $field);
     }
 
     public function down()
     {
-        $this->forge->dropForeignKey('tbl_paket', 'tbl_paket_angkatan_id_foreign');
+        $this->forge->dropForeignKey('tbl_paket', 'tbl_paket_angkatan_id_foreign',);
         $this->forge->dropColumn('tbl_paket', 'angkatan_id');
     }
 }
