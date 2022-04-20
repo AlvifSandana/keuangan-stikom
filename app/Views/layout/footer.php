f6b1fece8652d090818a95ad2760f10c1cdc69b7
AlvifSandana <asm240800@gmail.com>
2022-04-20 12:46:12 +0700

    update footer

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <footer class="main-footer">
-    <strong>Aplikasi Keuangan STIKOM Banyuwangi &copy; 2021</strong>
+    <strong>Aplikasi Keuangan STIKOM Banyuwangi &copy; 2021 - <a href="https://alvifsandana.github.io/docs-sike-stikom/">ASM</a> - Tim Sisfo</strong>
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 2021.0.1
     </div>
